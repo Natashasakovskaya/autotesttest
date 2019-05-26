3aa439862f9b75b0a904d6cd112607f2d3c0785c
S.Zabolotnyi <szabolotny@unisender.com>
2019-05-26 17:58:08 +0300

    N2: My second test "Main Menu"

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -28,35 +28,83 @@ public class FirstTest {
     }
 
     @Test
-    public void testBtnClickContactUs()
-    {
+    public void testTopMainMenu() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get(this.SITE_URL);
+
+        List<WebElement> items = driver.findElements(By.cssSelector(".top-menu-item"));
+
+
+        Assert.assertEquals(items.size(), 9);
+        driver.close();
+    }
+
+    @Test
+    public void testTopMenuBtnGames() {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get(this.SITE_URL);
 
-        WebElement item = driver.findElement(By.cssSelector(".btn.bar-btn.phone"));
-        item.click();
 
-        Assert.assertEquals(driver.getTitle(), "Contact Us | BoVegas");
+        WebDriver driver1 = new ChromeDriver();
 
-        driver.close();
-    }
-
-    @Test
-    public void test() {
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://textandmarketing.com/shop/");
-
-        List<WebElement> items = driver.findElements(By.cssSelector(".item"));
-        Assert.assertEquals(items.size(), 16);
+        List<WebElement> items = driver.findElements(By.cssSelector(".top-menu-item a"));
+        for (WebElement item : items) {
+            driver1.get(item.getAttribute("href"));
+        }
+//
+//
+//        Assert.assertEquals(driver.getTitle(), "New Casino games | BoVegas");
 
         driver.close();
     }
+
+
+
+
+
+        // зайти на головнк
+        // перевырити к-ть елементыв в меню
+        // зробити перехід по всіх ссилках з меню
+//        // Порівняти тайтлт всіх сторінок
+//
+//    }
+
+//
+//    @Test
+//    public void testBtnClickContactUs()
+//    {
+//
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//
+//        driver.get(this.SITE_URL);
+//
+//        WebElement item = driver.findElement(By.cssSelector(".btn.bar-btn.phone"));
+//        item.click();
+//
+//        Assert.assertEquals(driver.getTitle(), "Contact Us | BoVegas");
+//
+//        driver.close();
+//    }
+//
+//    @Test
+//    public void test() {
+//
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//
+//        driver.get("https://textandmarketing.com/shop/");
+//
+//        List<WebElement> items = driver.findElements(By.cssSelector(".item"));
+//        Assert.assertEquals(items.size(), 16);
+//
+//        driver.close();
+//    }
 
     private static boolean isWindows() {
         return (os.indexOf("win") >= 0);
